25bfde28961a8b0fc9c199f7ecd275b2b0875369
endzrijana <anastep1919@gmail.com>
2026-02-23 23:10:35 +0100

    Ispravke

--- frontend/src/pages/AdminDashboard/AdminDashboard.test.jsx
+++ frontend/src/pages/AdminDashboard/AdminDashboard.test.jsx
@@ -1,25 +1,41 @@
 import { render, screen } from '@testing-library/react';
-import AdminDashboard from './AdminDashboard';
 import { BrowserRouter } from 'react-router-dom';
+import AdminDashboard from './AdminDashboard';
+import { beforeEach, it, expect, vi } from 'vitest';
 
-test('Renderuje naslov Admin Dashboarda', () => {
-  localStorage.setItem('role', 'admin');
-  localStorage.setItem('token', 'fake-jwt-token');
+import axios from 'axios';
+vi.mock('axios');
+
+beforeEach(() => {
+  localStorage.clear();
+  vi.clearAllMocks();
+
+  localStorage.setItem('userRole', 'admin'); 
+  localStorage.setItem('token', 'fake-token');
+});
+
+it('Renderira naslov Admin Dashboarda', async () => {
+  axios.get.mockResolvedValue({ data: [] });
+
   render(
     <BrowserRouter>
       <AdminDashboard />
     </BrowserRouter>
   );
-  const titleElement = screen.getByText(/Upravljanje svim rezervacijama/i);
+
+  const titleElement = await screen.findByText(/Upravljanje svim rezervacijama/i);
   expect(titleElement).toBeInTheDocument();
 });
 
-test('Prikazuje poruku kada je lista rezervacija prazna', () => {
+it('Prikazuje poruku kada je lista rezervacija prazna', async () => {
+  axios.get.mockResolvedValue({ data: [] });
+
   render(
     <BrowserRouter>
       <AdminDashboard />
     </BrowserRouter>
   );
-  const noDataElement = screen.getByText(/Baza podataka je prazna/i);
+
+  const noDataElement = await screen.findByText(/Baza podataka je prazna/i);
   expect(noDataElement).toBeInTheDocument();
 });
