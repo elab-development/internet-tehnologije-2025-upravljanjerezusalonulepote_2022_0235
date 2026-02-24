f579620f6cf7ee638853f65c3b15b6d52c9066c8
endzrijana <anastep1919@gmail.com>
2026-02-24 01:37:09 +0100

    Ispravka

--- backend/config/database.js
+++ backend/config/database.js
@@ -12,7 +12,7 @@ const sequelize = new Sequelize(
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false
+        rejectUnauthorized: false 
       }
     },
     logging: false 
